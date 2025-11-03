cb7c226055c3b93352fb4fbcbdd696a8b7088225
stefanoantoniodonno <stefanoantonio.donno@marconibari.edu.it>
2025-11-03 18:07:18 +0000

    es2

--- Esercizio.java
+++ Esercizio.java
@@ -1,15 +1,49 @@
 
 public class Esercizio {
 
-    public static void riempiPerRighe(/* scrivere qui i parametri formali */) {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO
+    public static void riempiPerRighe(int[][] M, int righe, int colonne) {
+        int c = 1;
+        int i = 0;
+        while(i < righe){
+            int j = 0;
+            while(j < colonne){
+                M[i][j] = c;
+                ++c;
+                ++j;
+            }
+            ++i;
+        }
     }
 
-    public static void riempiPerColonne(/* scrivere qui i parametri formali */) {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO
+
+     public static void riempiPerColonne(int[][] M, int righe, int colonne) {
+        int c = 1;
+        int j = 0;
+        while(j < colonne){
+            int i = 0;
+            while(i < righe){
+                M[i][j] = c;
+                ++c;
+                ++i;
+            }
+            ++j;
+        }
     }
 
-    public static void main(String[] args) {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO
-    }
+
+   public static void main(String[] args) {
+        
+        int righe = 5;
+        int colonne = 5;
+
+        int[][] matricePerRighe = new int[righe][colonne];
+        int[][] matricePerColonne = new int[righe][colonne];
+
+        riempiPerRighe(matricePerRighe, righe, colonne);
+        UtilsMatrice.visualizza(matricePerRighe);
+
+        riempiPerColonne(matricePerColonne, righe, colonne);
+        UtilsMatrice.visualizza(matricePerColonne);
+    }
+
 }
